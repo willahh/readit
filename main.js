cdb4629eef546f8e563d34f7a0c64aef65fd2fa7
William Ravel <wravel@gmail.com>
2021-02-03 23:54:41 +0100

    Add new item creation

--- main.js
+++ main.js
@@ -1,40 +1,67 @@
 // Modules
-const { app, BrowserWindow } = require("electron");
+const { app, BrowserWindow, ipcMain } = require("electron");
+const windowStateKeeper = require("electron-window-state");
+
+console.log("Checking ready:", app.isReady());
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow;
 
+ipcMain.on("new-item", (e, itemUrl) => {
+  console.log(itemUrl);
+
+  setTimeout(() => {
+    e.sender.send("new-item-success", "new item from main process");
+  }, 2000);
+});
+
 // Create a new BrowserWindow when `app` is ready
 function createWindow() {
-  console.log("createWindow5");
+  let state = windowStateKeeper({
+    defaultWidth: 800,
+    defaultHeight: 650,
+  });
+  console.log("state", state);
   mainWindow = new BrowserWindow({
-    width: 1000,
-    height: 800,
+    x: state.x,
+    y: state.y,
+    width: state.width,
+    height: state.height,
+    minWidth: 350,
+    maxWidth: 650,
+    minHeight: 350,
     webPreferences: { nodeIntegration: true },
   });
+  mainWindow.loadFile("renderer/main.html");
 
-  // Load index.html into the new BrowserWindow
-  mainWindow.loadFile("index.html");
+  state.manage(mainWindow);
 
   // Open DevTools - Remove for PRODUCTION!
   mainWindow.webContents.openDevTools();
 
   // Listen for window being closed
-  mainWindow.on("closed", () => {
-    mainWindow = null;
-  });
+  // mainWindow.on("closed", () => {
+  //   mainWindow = null;
+  // });
 }
 
 // Electron `app` is ready
-app.on("ready", createWindow);
-
-// Quit when all windows are closed - (Not macOS - Darwin)
-app.on("window-all-closed", () => {
-  if (process.platform !== "darwin") app.quit();
+app.on("ready", () => {
+  console.log("App is ready !");
+  // console.log("desktop: ", app.getPath("desktop"));
+  // console.log("music: ", app.getPath("music"));
+  // console.log("temp: ", app.getPath("temp"));
+  // console.log("userData: ", app.getPath("userData"));
+  createWindow();
 });
 
-// When app icon is clicked and app is running, (macOS) recreate the BrowserWindow
-app.on("activate", () => {
-  if (mainWindow === null) createWindow();
-});
+// // Quit when all windows are closed - (Not macOS - Darwin)
+// app.on("window-all-closed", () => {
+//   if (process.platform !== "darwin") app.quit();
+// });
+
+// // When app icon is clicked and app is running, (macOS) recreate the BrowserWindow
+// app.on("activate", () => {
+//   if (mainWindow === null) createWindow();
+// });
---
+++ renderer/app.js
@@ -0,0 +1,50 @@
+const { ipcRenderer } = require("electron");
+
+let showModal = document.getElementById("show-modal");
+let closeModal = document.getElementById("close-modal");
+let modal = document.getElementById("modal");
+let addItem = document.getElementById("add-item");
+let itemUrl = document.getElementById("url");
+
+const toggleModalButton = () => {
+  if (addItem.disabled) {
+    addItem.disable = false;
+    addItem.style.opacity = 1;
+    addItem.innerText = "Add item";
+    closeModal.style.display = "inline";
+  } else {
+    addItem.disable = true;
+    addItem.style.opacity = 0.5;
+    addItem.innerText = "Adding...";
+    closeModal.style.display = "none";
+  }
+};
+
+showModal.addEventListener("click", (e) => {
+  modal.style.display = "flex";
+  itemUrl.focus();
+});
+
+closeModal.addEventListener("click", (e) => {
+  modal.style.display = "none";
+});
+
+addItem.addEventListener("click", (e) => {
+  if (itemUrl.value) {
+    ipcRenderer.send("new-item", itemUrl.value);
+    toggleModalButton();
+  }
+});
+
+itemUrl.addEventListener("keyUp", (e) => {
+  if (e.key == "Enter") {
+    addItem.click();
+  }
+});
+
+ipcRenderer.on("new-item-success", (e, data) => {
+  console.log("on:new-item-success", data);
+  toggleModalButton();
+  modal.style.display = "none";
+  itemUrl.value = "";
+});
